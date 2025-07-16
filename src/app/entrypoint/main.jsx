5d53c393380a31ee9368c246312ea6101413a35b
romantrukhtanov <romantrukhtanov@gmail.com>
2025-07-16 17:52:01 +0500

    add app and pages fsd layers

--- src/app/entrypoint/main.jsx
+++ src/app/entrypoint/main.jsx
@@ -1,8 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider } from 'react-router';
+
+import '../styles/index.css';
+import { routes } from '../routes';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <>FSD demo</>
+    <RouterProvider router={routes}/>
   </StrictMode>,
 )
---
+++ src/app/routes/routes.tsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from 'react-router';
+
+import { Layout } from '../layout';
+import { HomePage } from '../../pages';
+
+export const routes = createBrowserRouter([
+  {
+    path: '/',
+    Component: Layout,
+    children: [
+      {
+        index: true, element: <main>Main</main>
+      },
+      {
+        path: 'home', Component: HomePage,
+      }
+    ],
+  }
+]);
